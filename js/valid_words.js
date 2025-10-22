640a49b29e654bb38b7bb362fdc372b456161a86
mathursunit <sunit.mathur@gmail.com>
2025-10-22 16:54:20 -0400

    Initial commit: 8x Fun With Words v1.7.2

---
+++ js/valid_words.js
@@ -0,0 +1,10 @@
+(function(){
+  window.VALID_WORDS = [
+    "adieu","crane","slate","soare","stare","raise","trace","irate","alone","other",
+    "brain","candy","doubt","eager","frail","ghost","honey","apple","pearl","plead",
+    "pleat","ample","paler","panel","knead","kneel","sweet","sweep","swept","spear",
+    "spare","spore","spite","spire","spike","spoke","spook","spool","stool","stoic",
+    "stoop","snoop","snipe","shine","shire","shore","shone","phone","stone","tones",
+    "notes","onset","tenor","enter","ether"
+  ];
+})();
